f624ee3b991e8f06427db9785becfaa779244ae4
Amir Mohammed <amir.mohammed00@gmail.com>
2023-04-09 16:46:09 +0000

    done

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (const key of Object.keys(object)) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const findKey = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+};
+assertEqual(findKeyByValue(findKey, { stars: 1 }), "Blue Hill");
+assertEqual(findKeyByValue(findKey, { stars: 3 }), "Akaleri");
+assertEqual(findKeyByValue(findKey, { stars: 2 }), "noma");
+assertEqual(findKeyByValue(findKey, { stars: 4 }), undefined);
+
